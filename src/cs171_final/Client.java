471282cf1cbb7d2a577a113307d1d61a5428b1d6
Chris Kim <chriskim06@gmail.com>
2015-05-21 19:43:47 -0700

    Created new branch for CLI threads

---
+++ src/cs171_final/Client.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs171_final;
+
+/**
+ *
+ * @author Ricardo Morones <rmorones@umail.ucsb.edu>
+ * @author Chris Kim <chriskim06@gmail.com>
+ */
+public class Client {
+    
+}
